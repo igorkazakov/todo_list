15e148b57bd807fd124075eafae3f6ce9308489c
igorkazakov <igorkazakov777@yandex.ru>
2014-07-30 22:12:00 +0400

    Navigation Drawer menu

--- src/com/kaazak/todolist/MainActivity.java
+++ src/com/kaazak/todolist/MainActivity.java
@@ -3,12 +3,14 @@
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 public class MainActivity extends Activity
@@ -35,6 +37,46 @@ public class MainActivity extends Activity
 		drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
 		drawerList = (ListView)findViewById(R.id.list_slidermenu);
 		
+		navDrawerItems = new ArrayList<NavDrawerItem>();
+		
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
+		
+		navMenuIcons.recycle();
+		
+		adapter = new NavDrawerListAdapter(getApplicationContext(), 
+				navDrawerItems);
+		drawerList.setAdapter(adapter);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_launcher,
+				R.string.app_name,
+				R.string.app_name)
+		{
+			public void onDrawerClosed(View view)
+			{
+				getActionBar().setTitle(title);
+				invalidateOptionsMenu();
+			}
+			
+			public void onDrawerOpened(View view)
+			{
+				getActionBar().setTitle(drawerTitle);
+				invalidateOptionsMenu();
+			}
+		};
+		
+		drawerLayout.setDrawerListener(drawerToggle);
+		
+		if(savedInstanceState == null)
+		{
+			//displayView(0);
+		}
 	}
 
 	@Override
@@ -44,18 +86,44 @@ public class MainActivity extends Activity
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // toggle nav drawer on selecting action bar app icon/title
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action bar actions click
+        switch (item.getItemId()) {
+        case R.id.action_settings:
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+	
+	public boolean onPrepareOptionsMenu(Menu menu)
 	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) 
-		{
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
 	}
+	
+	public void setTitle(CharSequence title)
+	{
+		this.title = title;
+		getActionBar().setTitle(title);
+	}
+	
+	protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+ 
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 }
