708372272d9ceb5044e4a189eda48ca7e84b812b
igorkazakov <igorkazakov777@yandex.ru>
2014-07-31 19:33:22 +0400

    Navigation menu done

--- src/com/kaazak/todolist/MainActivity.java
+++ src/com/kaazak/todolist/MainActivity.java
@@ -3,14 +3,19 @@
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends Activity
@@ -49,12 +54,14 @@ public class MainActivity extends Activity
 		adapter = new NavDrawerListAdapter(getApplicationContext(), 
 				navDrawerItems);
 		drawerList.setAdapter(adapter);
+		drawerList.setOnItemClickListener(new SlideMenuClickListener());
+		
 		
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
 		
 		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
-				R.drawable.ic_launcher,
+				R.drawable.ic_drawer,
 				R.string.app_name,
 				R.string.app_name)
 		{
@@ -75,7 +82,7 @@ public class MainActivity extends Activity
 		
 		if(savedInstanceState == null)
 		{
-			//displayView(0);
+			displayView(0);
 		}
 	}
 
@@ -126,4 +133,50 @@ public class MainActivity extends Activity
         // Pass any configuration change to the drawer toggls
         drawerToggle.onConfigurationChanged(newConfig);
     }
+    
+    public class SlideMenuClickListener implements ListView.OnItemClickListener
+    {
+
+    	@Override
+    	public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+    	{
+    		displayView(position);
+    	}
+    }
+    
+    public void displayView(int position)
+	{
+		Fragment fragment = null;
+		switch(position)
+		{
+			case 0:
+				//fragment = new noteFragment();
+				break;
+			case 1:
+				//fragment = new reminderFragment();
+				break;
+			case 2:
+				//fragment = new informationFragment();
+				break;
+			case 3:
+				//fragment = new settingsFragment();
+				break;
+		}
+		
+		if(fragment != null)
+		{
+			FragmentManager fragmentManager = getFragmentManager();
+			fragmentManager.beginTransaction().
+				replace(R.id.frame_container, fragment).commit();
+			drawerList.setItemChecked(position, true);
+			drawerList.setSelection(position);
+			setTitle(navMenuTitles[position]);
+			drawerLayout.closeDrawer(drawerList);
+		} 
+		else
+		{
+			Log.e("MainActivity", "Error in creating fragment");
+        }
+		
+	}
 }
